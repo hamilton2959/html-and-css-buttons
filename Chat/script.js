bf562f8ffeddaa4bbe4561a8654ad365d77889e6
hamilton2959 <skytech2959@gmail.com>
2026-07-27 17:06:10 +0300

    init commit

---
+++ Chat/script.js
@@ -0,0 +1,31 @@
+const socket = io();
+
+const form = document.getElementById("chatForm");
+const input = document.getElementById("message");
+const messages = document.getElementById("messages");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    if (input.value) {
+
+        socket.emit("chat message", input.value);
+
+        input.value = "";
+
+    }
+
+});
+
+socket.on("chat message", function (msg) {
+
+    const li = document.createElement("li");
+
+    li.textContent = msg;
+
+    messages.appendChild(li);
+
+    messages.scrollTop = messages.scrollHeight;
+
+});
